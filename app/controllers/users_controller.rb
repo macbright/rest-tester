846b70dbd981f946a0e4ef2b2808ef793db1b8c6
Kelvin Liang <kelvin9877@gmail.com>
2019-07-05 23:10:50 +0800

    fix the post issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
     
+    def index
+        @name = "I am the Index action!"
+    end
+
     def new
         @name = "a user is being created"
     end
@@ -7,11 +11,14 @@ class UsersController < ApplicationController
     def show
         @name = "a user profile is show!"
     end
+
+    def create
+        @name = "a user is being created"
+    end
     
     def edit
         @name = "a user profile is editing!"
-    end
-      
+    end 
     
     def update
         @name = "user is updated"
@@ -21,7 +28,4 @@ class UsersController < ApplicationController
         @name = "user is removed"
     end
 
-    def index
-        @name = "I am the Index action!"
-    end
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -3,9 +3,9 @@ require 'rest-client'
 puts "Enter any URL"
 # url = gets.chomp()
 url = "http://localhost:3000/users"
-url1 = "http://localhost:3000/users/new"
+# url1 = "http://localhost:3000/users"
 
 
 # puts RestClient.get(url)
 
-RestClient.post(url1, "")
+RestClient.post(url, "")
